9269622147b25d9cfc39086eed5756b8194c9ab0
Gaetano Buscema <gaetano.buscema@eng.it>
2020-11-28 02:33:07 +0100

    [PULSBS-72]: added statistcs Cards style

--- client/src/components/StatisticsComponent.js
+++ client/src/components/StatisticsComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from "react-bootstrap/Card";
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import { FaUsers } from 'react-icons/fa';
+import { FaChalkboardTeacher, FaUser, FaUsers, FaBook } from 'react-icons/fa';
 
 
 const StatisticsComponent = (props) => {
@@ -10,66 +10,97 @@ const StatisticsComponent = (props) => {
     return <>
         <Col sm={1} />
         <Col sm={3}>
-            <Card>
-                <Card.Title>
-                    <b>Average students for each lesson</b>
-                </Card.Title>
+            <Card className="statisticCard">
                 <Card.Body>
                     <Row>
-                        <Col sm={4} />
-                        <Col sm={6}>
-                            {statistics && statistics.studentsCounts / statistics.nummberOfLessonPresence}
-                        </Col>
+                            <Col sm={4}>
 
-                        <Col sm={2}>
-                            <FaUsers></FaUsers>
+                                <h6 className="titleStatisticsCard" >REMOTE LESSONS</h6>
+                                <b><h3 className="contentStaticsCard" >5</h3></b>
+                                
+                            </Col>
+                            <Col sm={4}>
+                                
+                                <h6 className="titleStatisticsCard" >PRESENCE LESSONS</h6>
+                                <b><h3 className="contentStaticsCard" >1</h3></b>
+                                <div>{statistics && statistics.studentsCounts}</div>
+                            </Col>
+
+                        <Col sm={4}>
+                            <h6></h6>
+                            <div className="statIcon1"><FaChalkboardTeacher style={{
+                                    
+                                    "min-height": "30px",
+                                    "max-height": "30px"
+                                }} size="6x"></FaChalkboardTeacher></div>
                         </Col>
                     </Row>
                 </Card.Body>
             </Card>
         </Col>
 
-
-        <Col sm={2}>
-            <Card>
-                <Card.Header>
-                    Number of students booked
-            </Card.Header>
-                <Card.Body>
-                    <Row>
-                        <Col sm={4} />
-                        <Col sm={6}>
-                            {statistics && statistics.studentsCounts}
-                        </Col>
-                        <Col sm={2}>
-
-                        </Col>
- </Row>
-            </Card.Body>
-            </Card>
-        </Col>
-
             <Col sm={3}>
-                <Card>
-                    <Card.Header>
-                        Number of lessons
-            </Card.Header>
+                <Card className="statisticCard">
                     <Card.Body>
-                        {statistics && "Number of lessons: "+statistics.numberOfLessons} <br></br>
-                        {statistics && "Number of lessons cancelled: "+statistics.numberOfLessonsCancelled}
-                        
-            </Card.Body>
+                        <Row>
+                            <Col sm={4}>
+
+                                <h6 className="titleStatisticsCard" >LESSONS COUNT</h6>
+                                <b><h3 className="contentStaticsCard" >6</h3></b>
+                                
+                            </Col>
+                            <Col sm={4}>
+                                
+                                <h6 className="titleStatisticsCard" >LESSONS DELETED</h6>
+                                <b><h3 className="contentStaticsCard" >0</h3></b>
+                                <div>{statistics && statistics.studentsCounts}</div>
+                            </Col>
+
+                            <Col sm={4}>
+                                <h6></h6>
+                                <div className="statIcon3"><FaBook style={{
+                                    
+                                    "min-height": "30px",
+                                    "max-height": "30px"
+                                }} size="6x"></FaBook></div>
+                                
+                                
+                            </Col>
+                        </Row>
+                    </Card.Body>
                 </Card>
             </Col>
 
-            <Col sm={2}>
-                <Card>
-                    <Card.Header>
-                        Statistica 1
-            </Card.Header>
+            <Col sm={4}>
+                <Card className="statisticCard">
                     <Card.Body>
-                        Alcune statistiche
-            </Card.Body>
+                        <Row>
+                            <Col sm={4}>
+
+                                <h6 className="titleStatisticsCard" >STUDENT BOOKINGS</h6>
+                                <b><h3 className="contentStaticsCard" >6</h3></b>
+                                <div>{statistics && statistics.studentsCounts}</div>
+                                
+                            </Col>
+                            <Col sm={5}>
+
+                                <h6 className="titleStatisticsCard" >STUDENTS PER LECTURE</h6>
+                                <b><h3 className="contentStaticsCard" >12.5</h3></b>
+                                <div>{statistics && statistics.studentsCounts / statistics.nummberOfLessonPresence}</div>
+                                
+                            </Col>
+
+
+                            <Col sm={3}>
+                                <h6></h6>
+                                <div className="statIcon4"><FaUsers style={{
+                                        
+                                        "min-height": "30px",
+                                        "max-height": "30px"
+                                    }} size="6x"></FaUsers></div>
+                            </Col>
+                        </Row>
+                    </Card.Body>
                 </Card>
             </Col>
             <br></br>
